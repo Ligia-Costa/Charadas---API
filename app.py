e99c3d0cc9a38f0ad56de3a5c006a30efddfb733
Lígia Costa <ligia.costa.senai@gmail.com>
2025-04-08 10:21:31 -0300

    Nova rota - Listar charadas

--- app.py
+++ app.py
@@ -37,6 +37,20 @@ def charada_aleatoria():
         return jsonify(random.choice(charadas)), 200
     else:
         return jsonify({'mensagem': 'ERRO! Nenhuma charada encontrada.'}), 404
+    
+#Método GET -- Listar charadas
+@app.route('/charadas/lista', methods=['GET'])
+def charada_lista():
+    charadas = []
+    lista = db.collection('charadas').stream()
+    
+    for item in lista:
+        charadas.append(item.to_dict())
+
+    if charadas:
+        return jsonify(charadas), 200
+    else:
+        return jsonify({'mensagem': 'ERRO! Nenhuma charada encontrada.'}), 404
 
 #Método GET -- Charada por ID
 @app.route('/charadas/<id>', methods=['GET'])
